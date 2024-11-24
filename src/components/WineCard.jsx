30327d8738c8becae9ba1d13816a1298f2f82198
Sebastiaan Verbeek <ajsverbeek@gmail.com>
2024-11-24 09:41:28 +0100

    fix: Update wine value score display with currency symbol

--- src/components/WineCard.jsx
+++ src/components/WineCard.jsx
@@ -68,7 +68,8 @@ export default function WineCard({ wine, index, setSelectedImage, language = 'en
                 </span>
               </div>
               <div className="text-xs text-gray-600 dark:text-gray-400 flex items-center gap-0.5">
-                Value: {[...Array(Math.round(wine.valueScore))].map((_, i) => (
+                Value for €:{' '}
+                {[...Array(Math.round(wine.valueScore))].map((_, i) => (
                   <Star key={i} className="w-4 h-4 fill-yellow-400 text-yellow-400" />
                 ))}
               </div>
